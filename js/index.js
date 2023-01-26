e56ebabc5e451c34bb35011ae10dba32b69751f4
Leandro Pitarch <glpitarch@gmail.com>
2023-01-25 22:44:32 -0300

    profile, education and work experience information in phone dimension <768px was added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-/*----------///_To icons on dimension <768px_///----------*/
+/*----------///_DIMENSION <768px_///----------*/
+/*----------/_List icons_/----------*/
 profileIcon.addEventListener('click', () => { dataContainer("profileContainer") })
 educationIcon.addEventListener('click', () => { dataContainer("educationContainer") })
 workExperienceIcon.addEventListener('click', () => { dataContainer("workExperienceContainer") })
+/*----------/_Education icons_/----------*/
+webDeveloperPhoneTitle.addEventListener('click', () => { dataContainer("webDeveloperPhoneContainer") })
+javascriptPhoneTitle.addEventListener('click', () => { dataContainer("javascriptPhoneContainer") })
+othersPhoneTitle.addEventListener('click', () => { dataContainer("othersPhoneContainer") })
+technologistPhoneTitle.addEventListener('click', () => { dataContainer("technologistPhoneContainer") })
+workExperiencePhoneTitle.addEventListener('click', () => { dataContainer("workExperiencePhoneContainer") })
 
-/*----------///_To icons on dimension <768px_///----------*/
+/*----------///_To icons on dimension >768px_///----------*/
 profileIcon.addEventListener('click', tabletProfile)
 educationIcon.addEventListener('click', tabletEducation)
 workExperienceIcon.addEventListener('click', tabletWorkExperience)
