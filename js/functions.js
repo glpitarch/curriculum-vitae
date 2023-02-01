aa7813c799af234973547b7a8ad3ae32e707d02d
Leandro Pitarch <glpitarch@gmail.com>
2023-01-31 22:48:32 -0300

    Some animations was added on phone dimension

--- js/functions.js
+++ js/functions.js
@@ -9,24 +9,31 @@ const dataContainer = (id) => {
         }
 }
 
-
-
 const tabletProfile = () => { 
     document.getElementById('tabletContainer').innerHTML = `
-        <div >
-            <ul class="txt-left">
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
+        <div>
+            <ul class="white style-cleaner r-flex">
+                <li class="mx-3">
+                    <h4>Dirección de correo electrónico</h4>
+                    <div class="r-flex">
+                        <img src="./assets/images/icons/email.png" alt="Icono de email" width="50">
+                        <span class="align-self-center">glpitarch@gmail.com</span>
+                    </div>
+                </li>
+                <li class="mx-3">
+                    <h4>Número de teléfono celular</h4>
+                    <div class="r-flex">
+                        <img src="./assets/images/icons/whapp.png" alt="Icono de whatsapp" width="50">
+                        <span class="align-self-center">+54 9 11 3459-2877</span>
+                    </div>
+                </li>
+                <li class="mx-3">
+                    <h4>Ubicación</h4>
+                    <div class="r-flex">
+                        <img src="./assets/images/icons/location.png" alt="Icono de localización" width="50">
+                        <span class="align-self-center">Buenos Aires, Argentina</span>
+                    </div>
+                </li>
             </ul>
         </div>
     `
--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 /*----------///_DIMENSION <768px_///----------*/
-/*----------/_List icons_/----------*/
+/*----------/_List Icons_/----------*/
 profileIcon.addEventListener('click', () => { dataContainer("profileContainer") })
 educationIcon.addEventListener('click', () => { dataContainer("educationContainer") })
 workExperienceIcon.addEventListener('click', () => { dataContainer("workExperienceContainer") })
-/*----------/_Education icons_/----------*/
+/*----------/_Education Icons_/----------*/
 webDeveloperPhoneTitle.addEventListener('click', () => { dataContainer("webDeveloperPhoneContainer") })
 javascriptPhoneTitle.addEventListener('click', () => { dataContainer("javascriptPhoneContainer") })
 othersPhoneTitle.addEventListener('click', () => { dataContainer("othersPhoneContainer") })
 technologistPhoneTitle.addEventListener('click', () => { dataContainer("technologistPhoneContainer") })
-workExperiencePhoneTitle.addEventListener('click', () => { dataContainer("workExperiencePhoneContainer") })
+/*----------/_Work Experience_/----------*/
+technologistWorkExperiencePhoneTitle.addEventListener('click', () => { dataContainer("technologistWorkExperiencePhoneContainer") })
+developerWorkExperiencePhoneTitle.addEventListener('click', () => { dataContainer("developerWorkExperiencePhoneContainer") })
 
-/*----------///_To icons on dimension >768px_///----------*/
+/*----------///_DIMENSION >=768px_///----------*/
+/*----------///_List Icons_///----------*/
 profileIcon.addEventListener('click', tabletProfile)
 educationIcon.addEventListener('click', tabletEducation)
 workExperienceIcon.addEventListener('click', tabletWorkExperience)
