dd529ad05fb7307ba434c416190b15b18ade8770
Leandro Pitarch <glpitarch@gmail.com>
2023-02-05 21:40:23 -0300

    dynamic js code for large devices was added

--- js/functions.js
+++ js/functions.js
@@ -9,74 +9,169 @@ const dataContainer = (id) => {
         }
 }
 
-const tabletProfile = () => { 
-    document.getElementById('tabletContainer').innerHTML = `
-        <div>
-            <ul class="white style-cleaner r-flex">
-                <li class="mx-3">
-                    <h4>Dirección de correo electrónico</h4>
-                    <div class="r-flex">
-                        <img src="./assets/images/icons/email.png" alt="Icono de email" width="50">
-                        <span class="align-self-center">glpitarch@gmail.com</span>
-                    </div>
-                </li>
-                <li class="mx-3">
-                    <h4>Número de teléfono celular</h4>
-                    <div class="r-flex">
-                        <img src="./assets/images/icons/whapp.png" alt="Icono de whatsapp" width="50">
-                        <span class="align-self-center">+54 9 11 3459-2877</span>
-                    </div>
-                </li>
-                <li class="mx-3">
-                    <h4>Ubicación</h4>
-                    <div class="r-flex">
-                        <img src="./assets/images/icons/location.png" alt="Icono de localización" width="50">
-                        <span class="align-self-center">Buenos Aires, Argentina</span>
-                    </div>
-                </li>
-            </ul>
-        </div>
+const LargeDeviceProfile = () => {
+    educationLargeDeviceContainer.classList.remove('block')
+    educationLargeDeviceContainer.classList.add('hide')
+    workExperienceLargeDeviceContainer.classList.remove('block')
+    workExperienceLargeDeviceContainer.classList.add('hide')
+    profileLargeDeviceContainer.classList.remove('hide')
+    profileLargeDeviceContainer.classList.add('block')
+    document.getElementById('profileLargeDeviceContainer').innerHTML = `
+        <ul class="white style-cleaner c-flex appear">
+            <li class="mb-2 mt-1">
+                <h4 class="txt-center">Dirección de correo electrónico</h4>
+                <div class="r-flex">
+                    <img src="./assets/images/icons/email.png" alt="Icono de email" width="50">
+                    <span class="align-self-center mx-1">glpitarch@gmail.com</span>
+                </div>
+            </li>
+            <li class="my-2">
+                <h4 class="txt-center">Número de teléfono celular</h4>
+                <div class="r-flex">
+                    <img src="./assets/images/icons/whapp.png" alt="Icono de whatsapp" width="50">
+                    <span class="align-self-center mx-1">+54 9 11 3459-2877</span>
+                </div>
+            </li>
+            <li class="my-2">
+                <h4 class="txt-center">Ubicación</h4>
+                <div class="r-flex">
+                    <img src="./assets/images/icons/location.png" alt="Icono de localización" width="50">
+                    <span class="align-self-center mx-1">Buenos Aires, Argentina</span>
+                </div>
+            </li>
+        </ul>
     `
 }
 
-const tabletEducation = () => { 
-    document.getElementById('tabletContainer').innerHTML = `
-        <div >
-            <ul class="txt-left">
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-            </ul>
-        </div>
+const LargeDeviceEducation = () => { 
+    profileLargeDeviceContainer.classList.add('hide')
+    profileLargeDeviceContainer.classList.remove('block')
+    workExperienceLargeDeviceContainer.classList.add('hide')
+    workExperienceLargeDeviceContainer.classList.remove('block')
+    educationLargeDeviceContainer.classList.remove('hide')
+    educationLargeDeviceContainer.classList.add('block')
+    document.getElementById('webDeveloperLargeDeviceContainer').innerHTML = `
+        <p>Curso realizado en <a href="https://www.coderhouse.com/" class="white">Coderhouse</a></p>
+        <ul class="txt-left">
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/html.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>HTML 5</p>
+                </div>
+            </li>
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/css.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>CSS 3</p>
+                </div>
+            </li>
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/sass.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Sass</p>
+                </div>
+            </li>
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/bootstrap.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Bootstrap</p>
+                </div>
+            </li>
+        </ul>
+    `
+    document.getElementById('javascriptLargeDeviceContainer').innerHTML = `
+        <p>Curso realizado en <a href="https://www.coderhouse.com/" target="blank" class="white">Coderhouse</a></p>
+        <ul class="txt-left">
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/javascript.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Javascript</p>
+                </div>
+            </li>
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/react.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>React Js</p>
+                </div>
+            </li>
+        </ul>
+    `
+    document.getElementById('othersLargeDeviceContainer').innerHTML = `
+        <ul class="txt-left appear">
+            <p class="txt-center line-height-1 pb-1 mx-1">Curso de fundamentos básicos de la programación impulsado por el gobierno argentino <a href="https://www.argentina.gob.ar/produccion/argentina-programa/primera-etapa" target="blank" class="white">#SéProgramar:</a></p>
+            <h5 class="txt-center mt-2">Fundamentos:</h5>
+            <li class="r-flex justify-content-center py-1">
+                <img src="./assets/images/icons/gobstones.png" alt="Icono de CSS" width="50">
+                <div class="width-1 align-self-center">
+                    <p><a href="https://gobstones.github.io/" target="blank" class="white">Gobstones</a></p>
+                </div>
+            </li>
+            <h5 class="txt-center mt-2">Programación imperativa:</h5>
+            <li class="r-flex justify-content-center py-1">
+                <img src="./assets/images/icons/javascript.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Javascript</p>
+                </div>
+            </li>
+            <h5 class="txt-center mt-2">Programación con Objetos:</h5>
+            <li class="r-flex justify-content-center py-1">
+                <img src="./assets/images/icons/ruby.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Ruby</p>
+                </div>
+            </li>
+            <div class="bottom-border pb-1"></div>
+            <li class="r-flex justify-content-center my-2">
+                <img src="./assets/images/icons/git.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">
+                    <p>Git</p>
+                </div>
+            </li>
+        </ul>
+    `
+    document.getElementById('technologistLargeDeviceContainer').innerHTML = `
+        <ul class="txt-center mx-1 appear">
+            <li class="r-flex justify-content-center">
+                <a href="" class="align-self-center"><img src="./assets/images/icons/certificate.png" alt="Icono de diploma" width="50" class="pr-1"></a>
+                <div class="width-1 align-self-center">
+                    <p class="line-height-1">Técnico en diagnóstico por imágenes (2015)</p>
+                </div>
+            </li>
+            <li class="r-flex justify-content-center mt-2">
+                <a href="" class="align-self-center"><img src="./assets/images/icons/certificate.png" alt="Icono de diploma" width="50" class="pr-1"></a>
+                <div class="width-1 align-self-center">
+                    <p class="line-height-1">Licenciado en diagnóstico por imágenes con orientación en medicina nuclear (2022)</p>
+                </div>
+            </li>
+        </ul>
     `
 }
 
-const tabletWorkExperience = () => { 
-    document.getElementById('tabletContainer').innerHTML = `
-        <div >
-            <ul class="txt-left">
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                    Culpa voluptatibus odit tempore dolore magnam laudantium asperiores ipsa reiciendis unde, 
-                    vero corporis eum, nemo earum officia. Molestias voluptate veniam consequatur suscipit?</p></li>
-            </ul>
-        </div>
+const LargeDeviceWorkExperience = () => { 
+    profileLargeDeviceContainer.classList.add('hide')
+    profileLargeDeviceContainer.classList.remove('block')
+    educationLargeDeviceContainer.classList.add('hide')
+    educationLargeDeviceContainer.classList.remove('block')
+    workExperienceLargeDeviceContainer.classList.remove('hide')
+    workExperienceLargeDeviceContainer.classList.add('block')
+    document.getElementById('developerWorkExperienceLargeDeviceContainer').innerHTML = `
+        <ul class="txt-center mx-1 style-cleaner appear white">
+            <li class="r-flex justify-content-center my-2 appear">
+                <img src="./assets/images/icons/github.png" alt="Icono de CSS" width="50" class="pr-1">
+                <div class="width-1 align-self-center">                                           
+                    <p><a href="https://github.com/glpitarch?tab=repositories" target="blank" class="white">Github</a></p>
+                </div>
+            </li>
+        </ul>
+    `
+    document.getElementById('technologistWorkExperienceLargeDeviceContainer').innerHTML = `
+        <ul class="txt-center mx-1 style-cleaner appear white">
+            <li>
+                ARGUS Diagnóstico médico
+            </li>
+            <li class="my-2">
+                Centro de Educación Médica e Investigaciones Clínicas “Norberto Quirno” (CEMIC) (actualidad)
+            </li>
+        </ul>
     `
 }
--- js/index.js
+++ js/index.js
@@ -3,17 +3,25 @@
 profileIcon.addEventListener('click', () => { dataContainer("profileContainer") })
 educationIcon.addEventListener('click', () => { dataContainer("educationContainer") })
 workExperienceIcon.addEventListener('click', () => { dataContainer("workExperienceContainer") })
-/*----------/_Education Icons_/----------*/
+/*----------/_Education_/----------*/
 webDeveloperPhoneTitle.addEventListener('click', () => { dataContainer("webDeveloperPhoneContainer") })
 javascriptPhoneTitle.addEventListener('click', () => { dataContainer("javascriptPhoneContainer") })
 othersPhoneTitle.addEventListener('click', () => { dataContainer("othersPhoneContainer") })
 technologistPhoneTitle.addEventListener('click', () => { dataContainer("technologistPhoneContainer") })
 /*----------/_Work Experience_/----------*/
-technologistWorkExperiencePhoneTitle.addEventListener('click', () => { dataContainer("technologistWorkExperiencePhoneContainer") })
 developerWorkExperiencePhoneTitle.addEventListener('click', () => { dataContainer("developerWorkExperiencePhoneContainer") })
+technologistWorkExperiencePhoneTitle.addEventListener('click', () => { dataContainer("technologistWorkExperiencePhoneContainer") })
 
-/*----------///_DIMENSION >=768px_///----------*/
-/*----------///_List Icons_///----------*/
-profileIcon.addEventListener('click', tabletProfile)
-educationIcon.addEventListener('click', tabletEducation)
-workExperienceIcon.addEventListener('click', tabletWorkExperience)
+/*----------///_768px>= DIMENSION_///----------*/
+/*----------/_List Icons_/----------*/
+largeDeviceProfileIcon.addEventListener('click', LargeDeviceProfile)
+largeDeviceEducationIcon.addEventListener('click', LargeDeviceEducation)
+largeDeviceWorkExperienceIcon.addEventListener('click', LargeDeviceWorkExperience)
+/*----------/_Education_/----------*/
+webDeveloperLargeDeviceTitle.addEventListener('click', () => { dataContainer("webDeveloperLargeDeviceContainer") })
+javascriptLargeDeviceTitle.addEventListener('click', () => { dataContainer("javascriptLargeDeviceContainer") })
+othersLargeDeviceTitle.addEventListener('click', () => { dataContainer("othersLargeDeviceContainer") })
+technologistLargeDeviceTitle.addEventListener('click', () => { dataContainer("technologistLargeDeviceContainer") })
+/*----------/_Education_/----------*/
+developerWorkExperienceLargeDeviceTitle.addEventListener('click', () => { dataContainer("developerWorkExperienceLargeDeviceContainer") })
+technologistWorkExperienceLargeDeviceTitle.addEventListener('click', () => { dataContainer("technologistWorkExperienceLargeDeviceContainer") })
